f60345fb63aaef878268319e3922e24f73c13cb3
sachin2172003 <23203022@srcas.ac.in>
2024-07-01 12:22:21 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,9 +1,9 @@
 class sample{
   public static void main(String[]args)
   {
-    system.out.println("hello");
-        system.out.println("to");
-        system.out.println("devops");
-        system.out.println("lab");
+    System.out.println("hello");
+        System.out.println("to");
+        System.out.println("devops");
+        System.out.println("lab");
   }
 }
