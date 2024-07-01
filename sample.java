a0fd20f8ffe0da6ace9747048aed56217838f19f
sachin2172003 <23203022@srcas.ac.in>
2024-07-01 11:58:02 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,9 @@
+class sample{
+  public static void main(String[]args)
+  {
+    system.out.println("hello");
+        system.out.println("to");
+        system.out.println("devops");
+        system.out.println("lab");
+  }
+}
